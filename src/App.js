a9929ea877f41ef9301647ac5377e902312d1117
Igor Boyko <iboyko@gmail.com>
2021-06-02 19:40:47 -0700

    update lesson 4

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <h1>Online Catalogue</h1>
 
       <Catalog></Catalog>
+      <hr></hr>
 
       <Footer></Footer>
     </div>
